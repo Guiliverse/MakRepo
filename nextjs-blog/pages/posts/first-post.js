6b8a9afd779d8b6fc0f5085666edc3330b458c41
Guiliverse <guifreelacing2016@gmail.com>
2025-01-13 10:50:47 +0900

    add layout.module.css

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,12 +1,17 @@
 import Link from 'next/link';
 import Head from 'next/head';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
   return (
+
     <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
+      
+      
       <h1>First Post</h1>
       <h2>
         <Link href="/">Back to home</Link>
@@ -16,6 +21,7 @@ export default function FirstPost() {
           height={144}
           width={144}
           alt = "Your Name" />
+    </Layout>
     </>
   );
 }
