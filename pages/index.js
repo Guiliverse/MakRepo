d5c3fa0523c018505b7ac8a40ba9ee5a2c3c9b3b
Guiliverse <guifreelacing2016@gmail.com>
2025-01-15 05:33:41 +0900

    chaged impoting post route

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 
-import { getSortedPostsData } from '../lib/posts';
+import { getSortedPostsData } from "../lib/post.js";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
